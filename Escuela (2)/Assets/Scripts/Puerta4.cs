ff08825817d66b6e49e3298e13afd95012dfd866
ValentinNasi <54380321+ValentinNasi@users.noreply.github.com>
2019-09-01 01:13:42 -0300

    Cambio Radical
    Marque bien la U, Cambio del NPC, Comence a imaginar el preimer nivel

---
+++ Escuela (2)/Assets/Scripts/Puerta4.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta4 : MonoBehaviour
+{
+    public static bool doorKey;
+    public bool open;
+    public bool close;
+    public bool inTrigger;
+
+    void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+
+    void Update()
+    {
+        if (inTrigger)
+        {
+            if (close)
+            {
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    open = true;
+                    close = false;
+                }
+
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    close = true;
+                    open = false;
+                }
+            }
+        }
+
+        if (open)
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, -270.0f, 0.0f), Time.deltaTime * 200);
+            transform.rotation = newRot;
+        }
+        else
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, -180.0f, 0.0f), Time.deltaTime * 200);
+            transform.rotation = newRot;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (inTrigger)
+        {
+            if (open)
+            {
+                GUI.Box(new Rect(0, 0, 200, 25), "Presiona la tecla E para cerrar");
+            }
+            else
+            {
+                GUI.Box(new Rect(0, 0, 200, 25), "Presiona la tecla E para abrir");
+            }
+        }
+    }
+}
