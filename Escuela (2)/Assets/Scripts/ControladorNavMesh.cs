7cd5f12a8bd7412c3f17963353bf28a4d9148675
ValentinNasi <54380321+ValentinNasi@users.noreply.github.com>
2019-09-02 23:04:38 -0300

    Nivel1Comienzo

--- Escuela (2)/Assets/Scripts/ControladorNavMesh.cs
+++ Escuela (2)/Assets/Scripts/ControladorNavMesh.cs
@@ -8,7 +8,10 @@ public class ControladorNavMesh : MonoBehaviour
 {
     public NavMeshAgent agente;
     public Transform objetivo;
+    public float radioVision;
+    public float velocidad;
 
+    GameObject player;
 
     void Start()
     {
@@ -16,10 +19,30 @@ public class ControladorNavMesh : MonoBehaviour
         {
             agente = this.gameObject.GetComponent<NavMeshAgent>();
         }
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
     }
 
     void Update()
     {
         agente.SetDestination(objetivo.position);
+
+        float distancia = Vector3.Distance(player.transform.position, transform.position);
+
+        if (distancia < radioVision)
+        {
+            agente.SetDestination(player.transform.position);
+        }
+        else
+        {
+            agente.SetDestination(objetivo.position);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, radioVision);
     }
 }
---
+++ Escuela (2)/Assets/Scripts/InfoPantalla.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPantalla : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        
+
+        if(Input.GetKey(KeyCode.T))
+        {
+            
+            GUI.Box(new Rect(0, 0, 400, 25), "Busca la llave que te permita abrir el aula 1111");
+        }
+    }
+}
