4929422945bfd6e4e737aed8ea3c8e0db0d2d7a5
ValentinNasi <54380321+ValentinNasi@users.noreply.github.com>
2019-09-19 16:41:09 -0300

    Empece con nuevo nivel
    Empece a crear un nuevo nivel que consistiria en el gimnasio del colegio

---
+++ Escuela (2)/Assets/Scripts/PatearPelota.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatearPelota : MonoBehaviour
+{
+    public float fuerza;
+    public float x;
+    public Rigidbody pelota;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            pelota.gameObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(x, 0, 1) * fuerza, ForceMode.Impulse);
+        }
+    }
+   /* void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.tag == "Player")
+        {
+            pelota.gameObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(x, 0, 1) * fuerza, ForceMode.Impulse); 
+        }
+    }*/
+}
