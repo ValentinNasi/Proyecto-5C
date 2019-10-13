501865fdb30af7384a23a5e7c667e9858fb9be3f
valverdi <55467239+valverdi@users.noreply.github.com>
2019-10-12 22:31:14 -0300

    Animaciones Escuela y parte del tercer piso

---
+++ Escuela (2)/Assets/Scripts/LogicaAnimacion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaAnimacion : MonoBehaviour
+{
+    public float VelocidadMovimiento = 5.0f;
+     public float VelocidadRotacion = 200.0f;
+    private Animator anim;
+    public float x, y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //x = Input.GetAxis("Horizontal");
+        //y = Input.GetAxis("Vertical");
+    
+        
+    }
+
+}
