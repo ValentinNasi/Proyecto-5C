b66aacedd373bd9bf7b443a98571d8a64482da58
kelotrapani <54477736+kelotrapani@users.noreply.github.com>
2019-10-28 16:07:23 -0300

    menu de pausa andando
    nasi puto avisame q mas le pongo al menu de pausa

--- Escuela (2)/Assets/salir.cs
+++ Escuela (2)/Assets/salir.cs
@@ -5,18 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class salir : MonoBehaviour
 {
-    // Start is called before the first frame update
+    bool active;
+    Canvas canvas;
+    
+    
     void Start()
     {
-        
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        if (Input.GetKeyDown("escape"))
+        {
+            active = !active;
+            canvas.enabled = active;
+            Time.timeScale = (active) ? 0 : 1f;
+        }
 
     }
-   
-
+    public void volvermenu()
+    {
+        Application.Quit();
+        Debug.Log("se salio xd");
+    }
 }
+
+
+
